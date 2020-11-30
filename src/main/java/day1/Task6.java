c3476e6764e65326e6af875788d065c02e4c23de
Kirill Schegol <schegol.kirill@gmail.com>
2020-11-30 15:58:42 +0300

    complete day1 tasks

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 6;
+        for(int q = 1; q <= 9; q ++) {
+            System.out.println(q + " x " + k + " = " + (q * k) );
+        }
     }
 }
