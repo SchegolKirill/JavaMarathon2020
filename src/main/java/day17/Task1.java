b7a3e19260b9d64bc5c75d573d9c24e3bdcbc665
Kirill Schegol <schegol.kirill@gmail.com>
2020-12-20 21:06:39 +0300

    complete day17&18 tasks

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,19 @@
 package day17;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        List<ChessPiece> listOfChess = new ArrayList<>();
+        for(int i = 0; i <= 3; i++) {
+            listOfChess.add(ChessPiece.PAWN_WHITE);
+        }
+        for(int i = 0; i <= 3; i++) {
+            listOfChess.add(ChessPiece.ROOK_BLACK);
+        }
+        for(ChessPiece chess : listOfChess) {
+            System.out.print(chess + " ");
+        }
     }
 }
